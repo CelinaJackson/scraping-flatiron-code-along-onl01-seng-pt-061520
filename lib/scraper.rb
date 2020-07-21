2aaf4e4ad74308d56d9730658f1ec06fd3f14fe7
Celina Jackson <celinanjackson@gmail.com>
2020-07-21 23:16:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
@@ -11,16 +12,7 @@ class Scraper
     binding.pry
   end
 
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
+  
 
 end
  Scraper.new.get_page
